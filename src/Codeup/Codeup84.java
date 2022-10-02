2f0dd8d6a65a207ad8548da1a9252236e1b60a07
roal <replicaset01@gmail.com>
2022-10-03 00:02:56 +0900

    10.03 Commit

--- src/Codeup/Codeup84.java
+++ src/Codeup/Codeup84.java
@@ -9,21 +9,17 @@ public class Codeup84 {
 
         String[] num = br.readLine().split(" ");
 
-        int r = Integer.parseInt(num[0]);
-        int g = Integer.parseInt(num[1]);
-        int b = Integer.parseInt(num[2]);
         int c = 0;
 
-        for (int i=0; i<r; i++) {
-            for (int j=0; j<g; j++) {
-                for (int k=0; k<b; k++) {
-                    bw.write(String.format("%d, %d, %d", i, j, k));
-                    bw.newLine();
+        for (int i = 0; i<Integer.parseInt(num[0]); i++) {
+            for (int j = 0; j<Integer.parseInt(num[1]); j++) {
+                for (int k = 0; k<Integer.parseInt(num[2]); k++) {
+                    bw.write(String.format(i+" "+j+" "+k+"\n"));
                     c++;
                 }
             }
         }
-        bw.write(String.format("%d", c));
+        bw.write(String.valueOf(c));
         br.close();
         bw.close();
     }
