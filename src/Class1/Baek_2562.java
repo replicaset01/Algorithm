05ad0aae2911ebaa7aaffa003d5922fd4ca1bfc1
roal <replicaset01@github.com>
2022-10-06 21:51:36 +0900

    10.06 Commit

---
+++ src/Class1/Baek_2562.java
@@ -0,0 +1,13 @@
+package Class1;
+
+import java.io.*;
+
+public class Baek_2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] num = br.readLine().split("\n");
+
+    }
+}
