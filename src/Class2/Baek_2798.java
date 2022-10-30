04c61d8765ff0e24b653417dfa93f278d7b76cd4
roal <replicaset01@github.com>
2022-10-30 22:17:04 +0900

    a

--- src/Class2/Baek_2798.java
+++ src/Class2/Baek_2798.java
@@ -11,36 +11,37 @@ public class Baek_2798 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 첫번째 Tokenizer 객체 = 첫번째 줄의 입력을 받음
+        //i 1번째 Tokenizer 객체 = 카드 개수, 최대값을 입력받음
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        // 카드의 개수 n
+        //i 카드의 개수 n, 최대값 m, 들고있는 카드 개수 배열 card 선언
         int n = Integer.parseInt(st.nextToken());
-        // 최대값 m
         int m = Integer.parseInt(st.nextToken());
-        // 들고 있는 카드의 개수를 배열로 선언
         int[] card = new int[n];
-        // 2번째 Tokenizer 객체 = 카드의 숫자를 담음
+
+        //i 입력받는 값 사이에 개행 삽입, 카드의 숫자, max = 카드 3장의 숫자 합계
         st = new StringTokenizer(br.readLine(), " ");
-        // 카드 3장의 숫자 합계
         int max = 0;
-        // 입력받은 카드숫자를 카드 수만큼 배열의 요소에 넣기
+
+        //i 입력받은 카드의 숫자를 카드개수(n)만큼 배열(card)의 요소에 넣기
         for (int i=0; i<n; i++) {
             card[i] = Integer.parseInt(st.nextToken());
         }
-        // 배열 오름차순 정렬
+
+        //i 배열 오름차순 정렬
         Arrays.sort(card);
-        // 정렬된 배열에서 3번째로 큰수
+        //i 순서가 +1씩 차이나는 요소 순회
         for (int i=0; i<card.length-2; i++) {
-            // 정렬된 배열에서 2번째로 큰수
             for (int j=i+1; j<card.length-1; j++) {
-                // 정렬된 배열에서 1번째로 큰수
                 for (int k=j+1; k<card.length; k++) {
                     int sum=0;
-                    // 3개의 요소를 순회하면서 더함
+
+                    //i 3개의 요소를 순회하면서 더함
                     sum = sum+ card[i] + card[j] + card[k];
-                    // 3개 값을 더한 합이 m에 가까워질때까지 3개의 i,j,k 요소들을 합함
+
+                    //i 3개 값을 더한 합이 m에 가까워질때까지 3개의 i,j,k 요소들을 합함
                     if (sum <= m) {
-                        // max,sum중 더 큰값을 max 변수에 저장
+
+                        //i max,sum중 더 큰값을 max 변수에 저장
                         max = Math.max(max, sum);
                     }
                 }
--- src/Class2/Baek_4153.java
+++ src/Class2/Baek_4153.java
@@ -14,14 +14,14 @@ public class Baek_4153 {
 
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            // 세 변 입력받음
+            //i 세 변 입력받음
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             int z = Integer.parseInt(st.nextToken());
 
-            // 0 0 0 입력받으면 종료
+            //i 마지막줄 = 0 0 0 입력받으면 종료
             if (x==0 && y==0 && z==0) break;
-            // 세 변중 어느 변이 제일 큰지 모르니 모든 경우의수를 조건에 추가
+            //i 세 변중 어느 변이 제일 큰지 모르니 모든 경우의수를 조건에 추가
             if ((x*x + y*y) == z*z) {
                 sb.append("right").append("\n");
             } else if (x*x == (y*y + z*z)) {
