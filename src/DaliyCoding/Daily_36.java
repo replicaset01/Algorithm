7d976371948e5db9e485ef56dbe5c04424e9f1c8
roal <replicaset01@github.com>
2022-11-14 09:48:56 +0900

    a

--- src/DaliyCoding/Daily_36.java
+++ src/DaliyCoding/Daily_36.java
@@ -17,10 +17,12 @@ public class Daily_36 {
     //   return arr;
     // }
 
-    /** 최적화를 적용한 코드
+    /**
+     * 최적화를 적용한 코드
      * 삽입정렬에서 기존 정렬 과정에서 이미 정렬이 이루어진 범위보다 큰 수를 비교할 경우,
      * 앞서서 이미 정렬한 숫자와 비교할 필요가 없습니다.
-     * 불필요한 과정을 줄일 수 있는 최적화 과정이 적용된 코드입니다. **/
+     * 불필요한 과정을 줄일 수 있는 최적화 과정이 적용된 코드입니다.
+     **/
 
     public int[] insertionSort(int[] arr) {
         for (int index = 1; index < arr.length; index++) {
@@ -32,5 +34,6 @@ public class Daily_36 {
                 target--;
             }
         }
-
+        return arr;
     }
+}
